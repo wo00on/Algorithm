82e485df8e4eb946b2d86ccbb13c020c568896a3
wo00on <woony8959@gmail.com>
2025-02-03 16:31:39 +0900

    사분면

---
+++ 단계별로풀어보기/사분면.py
@@ -0,0 +1,15 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+if a > 0 and b > 0:
+    print('1')
+
+elif a < 0 and b > 0:
+    print('2')
+
+elif a < 0 and b < 0:
+    print('3')
+
+else : 
+    print('4')
