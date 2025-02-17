f00fce2b641c5a23449180deb3f7dbd876850709
wo00on <woony8959@gmail.com>
2025-02-17 21:14:05 +0900

    A+B-7

--- 단계별로풀어보기/A+B-7.py
+++ 단계별로풀어보기/A+B-7.py
@@ -1,6 +1,19 @@
 t = int(input())
 
-for i in range(t):
+for i in range(1, t+1):
     a, b = map(int,input().split())
     
-    print("Case #",i + 1,':', a + b)
+    print("Case #"+ str(i) + ':', a + b)
+
+
+print('-------------------------------------')
+
+t = int(input())
+
+for i in range(1, t+1):
+    a, b = map(int,input().split())
+    
+    print("Case #"+ str(i), ':', a + b)
+
+    
+print('-------------------------------------')
