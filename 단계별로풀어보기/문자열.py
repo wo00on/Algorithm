d88f3ffd4bf3e0f433a3a9da30df05b02aea33cd
wo00on <woony8959@gmail.com>
2025-02-18 21:17:15 +0900

    아스키

--- 단계별로풀어보기/문자열.py
+++ 단계별로풀어보기/문자열.py
@@ -5,3 +5,14 @@ for _ in range(T):
     print(char[0] + char[-1])
 
     
+
+####################################
+
+T = int(input())  # 입력 횟수 입력받기
+results = []  # 결과를 저장할 리스트
+
+for _ in range(T):
+    char = input() * 3  # 문자열 3번 반복
+    results.append(char[0] + char[-1])  # 처음과 마지막 문자 저장
+
+print("\n".join(results))  # 한 번에 출력
---
+++ 단계별로풀어보기/아스키.py
@@ -0,0 +1,4 @@
+char = input()
+
+print(ord(char))
+
